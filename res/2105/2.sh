e06a77db25eed5f106c60f8f74cabd23236914af
Egor Kliuev <user.email=egor.kliuev@bilateral.group>
2022-04-08 00:33:15 +0300

    33333333333333333

--- res/2105/2.sh
+++ res/2105/2.sh
@@ -24,28 +24,7 @@ done
 #git config --global user.name "Egor Kliuev"
 #git config --global user.email "user.email=egor.kliuev@bilateral.group"
 #Pull request#
-if [ $(date +%w) = $PR_DAY ]; then
-	git checkout -b $SOURCE_BRANCH
-			echo "checkouted succ"
-
-	for var in $response
-	do(git add $var.xml)
-	done
-
-	echo "git add succ"
-
-	git commit -a -m "Jenkins configs backup from: $TODAY_DATE"
-	echo "git commit succ"
 
   cat /home/egor/.ssh/id_rsa
-	git push origin $SOURCE_BRANCH
-	echo "push succ"
-
-	curl https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACEID/$BITBUCKET_REPO/pullrequests \
-	    -u egorkluev:CEQcYMprjy2y5H7PcUFs \
-	    --request POST \
-	    --header 'Content-Type: application/json' \
-	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'$TARGET_BRANCH'"}}}'
-	echo "pr succ"
-fi
+ssh -T git@bitbucket.org
 
