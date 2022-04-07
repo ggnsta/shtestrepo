f1c7d433895abab72b5bb83432696fce605c0c1c
Egor Kliuev <user.email=egor.kliuev@bilateral.group>
2022-04-07 16:32:01 +0300

    33333333333333333

--- res/2105/working.sh
+++ res/2105/working.sh
@@ -3,50 +3,45 @@
 JENKINS_IP="http://localhost:8080"
 JENKINS_USERNAME="egor"
 JENKINS_PASSWORD="123456"
-FORMAT=".xml"
 ##############################
 ###### BITBUCKET CONFIG ######
 BITBUCKET_USERNAME="egorkluev"
 BITBUCKET_PASSWORD="CEQcYMprjy2y5H7PcUFs"
 BITBUCKET_WORKSPACEID="egorkluev"
 BITBUCKET_REPO="shtestrepo"
+TARGET_BRANCH=develop
 ###################################
 JENKINS_VIEW="1. VKP - D"
-PR_DAY="4"
-
-#bilekl qwasGhvx33s 
-#bilavi Welcome.2
-#http://172.29.45.21:8080
-#http://172.29.45.172:8080
-
+PR_DAY="5"
+BRANCH_NAME_PATTERN=JenkinsJobConfigBackUp/JenkinsBackup
 
 TODAY_DATE=$(date +"%m-%d-%Y")
-BRANCH_NAME_PATTERN=JenkinsJobConfigBackUp/JenkinsBackup
-BRANCH=$BRANCH_NAME_PATTERN-$TODAY_DATE
+SOURCE_BRANCH=$BRANCH_NAME_PATTERN-$TODAY_DATE
+
 
 response=$(java -jar jenkins-cli.jar -s $JENKINS_IP -auth $JENKINS_USERNAME:$JENKINS_PASSWORD list-jobs "$JENKINS_VIEW")
 
 for var in $response
-do (java -jar jenkins-cli.jar -s $JENKINS_IP -auth $JENKINS_USERNAME:$JENKINS_PASSWORD get-job $var > $var$FORMAT)
+do (java -jar jenkins-cli.jar -s $JENKINS_IP -auth $JENKINS_USERNAME:$JENKINS_PASSWORD get-job $var > $var.xml)
 done
 
 
 
 #PULL REQUEST#
 if [ $(date +%w) = $PR_DAY ]; then
-	git checkout -b $BRANCH
+	git checkout -b $SOURCE_BRANCH
 
 	for var in $response
-	do(git add $var$FORMAT)
+	do(git add $var$.xml)
 	done
 
 	git commit -a -m "Jenkins configs backup from: $TODAY_DATE"
 
-	git push origin $BRANCH
+	git push origin $SOURCE_BRANCH
 
 	curl https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACEID/$BITBUCKET_REPO/pullrequests \
 	    -u egorkluev:CEQcYMprjy2y5H7PcUFs \
 	    --request POST \
 	    --header 'Content-Type: application/json' \
-	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$BRANCH'"}}, "destination": {"branch": {"name": "'develop'"}}}'
+	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'$TARGET_BRANCH'"}}}'
 fi
