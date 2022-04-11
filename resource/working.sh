a4b06515e0aaba6f145188817feeb35115398d01
Jenkins <jenkins@egor-MS-7B84>
2022-04-11 12:28:45 +0300

    222222222222222222222222

--- resource/working.sh
+++ resource/working.sh
@@ -5,7 +5,7 @@ echo "2222222222222222222"
 echo $JENKINS_URL
 echo $JENKINS_USERNAME
 echo $JENKINS_PASSWORD
-echo "$JENKINS_VIEW"
+echo $JENKINS_VIEW
 echo "2222222222222222222"
 response=$(java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD list-jobs "$JENKINS_VIEW")
 
@@ -29,5 +29,5 @@ if [ $(date +%w) = $PR_DAY ]; then
 	    -u $BITBUCKET_USERNAME:$BITBUCKET_PASSWORD \
 	    --request POST \
 	    --header 'Content-Type: application/json' \
-	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'$GIT_BRANCH'"}}}'
+	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'${GIT_BRANCH:7}'"}}}'
 fi
