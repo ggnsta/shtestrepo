9b62ba4e824ca861a30b2f016e19dc0219c122ab
Jenkins <jenkins@egor-MS-7B84>
2022-04-11 09:48:09 +0300

    чета начало работать5\7

---
+++ resource/working.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+TODAY_DATE=$(date +"%m-%d-%Y")
+SOURCE_BRANCH=$BRANCH_NAME_PATTERN-$TODAY_DATE
+
+
+response=$(java -jar jenkins-cli.jar -s $JENKINS_IP -auth $JENKINS_USERNAME:$JENKINS_PASSWORD list-jobs "$JENKINS_VIEW")
+
+for var in $response
+do (java -jar jenkins-cli.jar -s $JENKINS_IP -auth $JENKINS_USERNAME:$JENKINS_PASSWORD get-job $var > 2105/$var.xml)
+done
+
+#PULL REQUEST#
+if [ $(date +%w) = $PR_DAY ]; then
+	git checkout -b $SOURCE_BRANCH
+
+	for var in $response
+	do(git add $var.xml)
+	done
+
+	git commit -a -m "Jenkins configs backup from: $TODAY_DATE"
+
+	git push origin $SOURCE_BRANCH
+
+	curl https://api.bitbucket.org/2.0/repositories/$BITBUCKET_WORKSPACEID/$BITBUCKET_REPO/pullrequests \
+	    -u $BITBUCKET_USERNAME:$BITBUCKET_PASSWORD \
+	    --request POST \
+	    --header 'Content-Type: application/json' \
+	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'$TARGET_BRANCH'"}}}'
+fi
