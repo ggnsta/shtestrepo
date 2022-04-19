1f62eff1e7732542a7f6ebb287acebd0e15645da
Egor Kliuev <egor.kliuev@bilateral.group>
2022-04-19 15:46:17 +0300

    555

--- resource/working.sh
+++ resource/working.sh
@@ -65,6 +65,7 @@ for var in $response
 do (java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD get-job $var > 2105/$var.xml)
 done
 
+echo $BITBUCKET_PASSWORD
 echo "https://$BITBUCKET_USERNAME:$BITBUCKET_PASSWORD${GIT_URL:9}"
 git remote set-url origin https://$BITBUCKET_USERNAME:$BITBUCKET_PASSWORD${GIT_URL:9}
 
