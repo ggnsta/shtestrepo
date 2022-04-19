029cef47525b74185e63feb984774cf651a59ddc
Egor Kliuev <egor.kliuev@bilateral.group>
2022-04-19 11:07:09 +0300

    4 4 4

--- resource/working.sh
+++ resource/working.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+BITBUCKET_USERNAME=$1
+BITBUCKET_PASSWORD=$2
+GIT_URL=$3
+JENKINS_USERNAME=$4
+JENKINS_PASSWORD=$5
+JENKINS_URL=$6
+JENKINS_VIEW=$7
+SOURCE_BRANCH=$8
+TARGET_BRANCH=$9
+PR_DAY=${10}
+BRANCH_NAME_PATTERN=${11}
 
-if [[ -z "${BRANCH_NAME_PATTERN}" ]]; then
-  echo "variable BRANCH_NAME_PATTERN is undefined. Script exits with an error."
+if [[ -z "${BITBUCKET_USERNAME}" ]]; then
+  echo "variable BITBUCKET_USERNAME is undefined. Script exits with an error."
   exit 1
 fi
-if [[ -z "${PR_DAY}" ]]; then
-  echo "variable PR_DAY is undefined. Script exits with an error."
+if [[ -z "${BITBUCKET_PASSWORD}" ]]; then
+  echo "variable BITBUCKET_PASSWORD is undefined. Script exits with an error."
+  exit 1
+fi
+if [[ -z "${GIT_URL}" ]]; then
+  echo "variable GIT_URL is undefined. Script exits with an error."
   exit 1
 fi
 if [[ -z "${JENKINS_USERNAME}" ]]; then
@@ -16,22 +31,33 @@ if [[ -z "${JENKINS_PASSWORD}" ]]; then
   echo "variable JENKINS_PASSWORD is undefined. Script exits with an error."
   exit 1
 fi
+if [[ -z "${JENKINS_URL}" ]]; then
+  echo "variable JENKINS_URL is undefined. Script exits with an error."
+  exit 1
+fi
 if [[ -z "${JENKINS_VIEW}" ]]; then
   echo "variable JENKINS_VIEW is undefined. Script exits with an error."
   exit 1
 fi
-if [[ -z "${BITBUCKET_USERNAME}" ]]; then
-  echo "variable BITBUCKET_USERNAME is undefined. Script exits with an error."
+if [[ -z "${SOURCE_BRANCH}" ]]; then
+  echo "variable SOURCE_BRANCH is undefined. Script exits with an error."
   exit 1
 fi
-if [[ -z "${BITBUCKET_PASSWORD}" ]]; then
-  echo "variable BITBUCKET_PASSWORD is undefined. Script exits with an error."
+if [[ -z "${TARGET_BRANCH}" ]]; then
+  echo "variable SOURCE_BRANCH is undefined. Script exits with an error."
+  exit 1
+fi
+if [[ -z "${PR_DAY}" ]]; then
+  echo "variable PR_DAY is undefined. Script exits with an error."
+  exit 1
+fi
+if [[ -z "${BRANCH_NAME_PATTERN}" ]]; then
+  echo "variable BRANCH_NAME_PATTERN is undefined. Script exits with an error."
   exit 1
 fi
 
 TODAY_DATE=$(date +"%m-%d-%Y")
 SOURCE_BRANCH=$BRANCH_NAME_PATTERN-$TODAY_DATE
-
 
 response=$(java -jar jenkins-cli.jar -s $JENKINS_URL -auth $JENKINS_USERNAME:$JENKINS_PASSWORD list-jobs "$JENKINS_VIEW")
 
@@ -60,5 +86,5 @@ if [ $(date +%w) = $PR_DAY ]; then
 	    -u $BITBUCKET_USERNAME:$BITBUCKET_PASSWORD \
 	    --request POST \
 	    --header 'Content-Type: application/json' \
-	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'${GIT_BRANCH:7}'"}}}'
+	    --data '{"title": "JB-'$TODAY_DATE'","source": {"branch": {"name": "'$SOURCE_BRANCH'"}}, "destination": {"branch": {"name": "'${TARGET_BRANCH:7}'"}}}'
 fi
