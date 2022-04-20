a8e4e6b4999b1da7f3f800a3963d13e14aa35739
Egor Kliuev <egor.kliuev@bilateral.group>
2022-04-20 13:17:58 +0300

    555

--- resource/working.sh
+++ resource/working.sh
@@ -77,6 +77,9 @@ pull_github(){
     -H "Authorization: token $VCS_PASSWORD" \
     https://api.github.com/repos/$VCS_WORSPACE/$VCS_REPO/pulls \
     -d '{"title":"'$COMMIT_MSG-$TODAY_DATE'","body":"","head":"'$VCS_WORSPACE':'$SOURCE_BRANCH'","base":"'${TARGET_BRANCH:7}'"}'
+
+    echo "https://api.github.com/repos/$VCS_WORSPACE/$VCS_REPO/pulls"
+    echo '{"title":"'$COMMIT_MSG-$TODAY_DATE'","body":"","head":"'$VCS_WORSPACE':'$SOURCE_BRANCH'","base":"'${TARGET_BRANCH:7}'"}'
 }
 
 pull_bitbucket(){
