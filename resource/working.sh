6bce14f764183f42e6df4ff07aaf67ba42f7ab0f
Egor Kliuev <egor.kliuev@bilateral.group>
2022-04-20 10:41:04 +0300

    555

--- resource/working.sh
+++ resource/working.sh
@@ -71,8 +71,6 @@ parse_git_url(){
 }
 
 pull_github(){
-  echo " https://api.github.com/repos/$VCS_WORSPACE/$VCS_REPO/pulls \
-            -d '{"title":"'$COMMIT_MSG-$TODAY_DATE'","body":"","head":"'$VCS_WORSPACE':'$SOURCE_BRANCH'","base":"'${TARGET_BRANCH:7}'"}'"
   curl -s -o response.txt -w "%{http_code}"
     -X POST \
     -H "Accept: application/json" \
